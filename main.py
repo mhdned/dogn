a9dd292f7cfa946f975b352a61328fc616034dbd
Mehtiuo <mahdi.nedaee4@gmail.com>
2024-04-27 19:30:10 +0330

    ADD cors config to project

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # routings
 from apis.routers.index import router as Rooting
@@ -12,5 +13,17 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(app, host=config["HOST"], port=config["PORT"])
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(Rooting)
